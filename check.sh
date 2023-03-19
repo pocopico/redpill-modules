0dd8bcc81f62b7552496e14607bccdc5c7bf0c32
pocopico <pasafiogos@gmail.com>
2023-03-19 11:52:35 +0200

    Added new compiled modules

--- check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in `cat all_modules `
-do 
-ls $file/Makefile > /dev/null || echo "$file Error" && echo "$file -> OK !"
-done
-
--- hpsa/5.x/hpsa.mod
+++
@@ -1,2 +0,0 @@
-/opt/dsm-kernel/epyc7002/redpill-modules/hpsa/5.x/hpsa.o
-
